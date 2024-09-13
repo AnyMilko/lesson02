3b93dcb78c476d67bdc7805b60dc6e19f9f9d13a
AnyMilko <milko.ann@mail.ru>
2024-09-13 18:12:20 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+number_of_homework_completed = '12'
+Hour = '1.5'
+Name = 'Python'
+Tame = (1.5 / 12)
+print('Курс:', Name,'всего задачь:', number_of_homework_completed, 'затраченно часов:', Hour, 'среднее время исполнения', Tame)
